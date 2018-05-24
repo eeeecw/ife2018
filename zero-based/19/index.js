ebc32d83bab1a4e60a419e9ef3fa3f02dafde1f2
eeeecw <eeeecw@163.com>
2018-05-24 17:01:23 +0800

    19
    zero day19

---
+++ zero-based/19/index.js
@@ -0,0 +1,17 @@
+function getAllListItem() {
+    // 返回页面中所有li标签
+    var matches = document.body.querySelectorAll('p');
+    console.log(matches);
+}
+
+function findAllHtmlSpanInOneSection(sectionId) {
+    // 返回某个section下所有span中内容为HTML的span标签
+}
+
+function findListItem(sectionId, spanCont) {
+    // 返回某个section下，所有所包含span内容为spanCont的LI标签
+}
+
+function getActiveLinkContent(sectionId) {
+    // 返回某个section下，class为active的链接中包含的文字内容
+}
